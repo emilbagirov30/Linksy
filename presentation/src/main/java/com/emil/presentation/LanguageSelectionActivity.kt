d6445f00e91be9b8b1c1b3dfb2ae33b00b48a1b6
emilbagirov30 <emil_bgr@mail.ru>
2024-10-21 16:59:14 +0300

    added language selection

--- presentation/src/main/java/com/emil/presentation/LanguageSelectionActivity.kt
+++ presentation/src/main/java/com/emil/presentation/LanguageSelectionActivity.kt
@@ -1,20 +1,22 @@
 package com.emil.presentation
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class LanguageSelectionActivity : AppCompatActivity() {
+    private lateinit var next: MaterialButton
+    private lateinit var languageSelector: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_language_selection)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        next = findViewById(R.id.bt_next)
+        languageSelector = findViewById(R.id.language_spinner)
+
     }
 }
