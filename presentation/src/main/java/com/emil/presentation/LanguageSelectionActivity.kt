e744e186ce79cfd1053bb7f8b277931ea58a9a90
emilbagirov30 <emil_bgr@mail.ru>
2024-10-21 15:15:22 +0300

    added an activity for language selection

--- presentation/src/main/java/com/emil/presentation/LanguageSelectionActivity.kt
+++ presentation/src/main/java/com/emil/presentation/LanguageSelectionActivity.kt
@@ -1,26 +1,20 @@
 package com.emil.presentation
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@SuppressLint("CustomSplashScreen")
-class SplashScreenActivity : AppCompatActivity() {
+class LanguageSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_language_selection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val switchingToLanguageSelectionActivity= Intent(this, LanguageSelectionActivity::class.java)
-        startActivity(switchingToLanguageSelectionActivity)
-        finish()
     }
 }
