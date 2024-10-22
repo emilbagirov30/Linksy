408d3c4306bf325d41fb1546f7d5c039db9d81d7
emilbagirov30 <emil_bgr@mail.ru>
2024-10-22 19:15:46 +0300

    fixed the activity names in the manifest

--- presentation/src/main/java/com/emil/presentation/ui/AuthActivity.kt
+++ presentation/src/main/java/com/emil/presentation/ui/AuthActivity.kt
@@ -1,10 +1,8 @@
 package com.emil.presentation.ui
 import com.emil.presentation.R
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
