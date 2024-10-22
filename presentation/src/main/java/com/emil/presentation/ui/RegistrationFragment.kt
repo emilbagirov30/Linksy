54b64384904f7feec8686df7cbb8f4a8b8a376b2
emilbagirov30 <emil_bgr@mail.ru>
2024-10-22 20:40:24 +0300

    added a function to show and hide the password

--- presentation/src/main/java/com/emil/presentation/ui/RegistrationFragment.kt
+++ presentation/src/main/java/com/emil/presentation/ui/RegistrationFragment.kt
@@ -1,10 +1,12 @@
 package com.emil.presentation.ui
 
+import android.annotation.SuppressLint
 import com.emil.presentation.R
 import com.emil.presentation.utils.replaceFragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import com.emil.presentation.utils.string
+import com.emil.presentation.utils.togglePasswordVisibility
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textview.MaterialTextView
 
@@ -21,17 +24,17 @@ class RegistrationFragment : Fragment() {
     private lateinit var usernameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var passwordEditText: EditText
-    private lateinit var confirmEditText: EditText
+    private lateinit var  passwordConfirmEditText: EditText
     private lateinit var signUpButton: MaterialButton
-    private lateinit var showPasswordButton: ImageView
-    private lateinit var showPasswordConfirmButton: ImageView
-    private lateinit var hidePasswordButton: ImageView
-    private lateinit var hidePasswordConfirmButton: ImageView
+    private lateinit var changeInputTypePasswordButton: ImageView
+    private lateinit var changeInputTypePasswordConfirmButton: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,12 +44,11 @@ class RegistrationFragment : Fragment() {
         usernameEditText = view.findViewById(R.id.et_username)
         emailEditText = view.findViewById(R.id.et_email)
         passwordEditText = view.findViewById(R.id.et_password)
-        confirmEditText = view.findViewById(R.id.et_confirm_password)
+        passwordConfirmEditText = view.findViewById(R.id.et_confirm_password)
         signUpButton = view.findViewById(R.id.bt_sign_up)
-        showPasswordButton = view.findViewById(R.id.iv_password_show)
-        showPasswordConfirmButton = view.findViewById(R.id.iv_passwordc_show)
-        hidePasswordButton = view.findViewById(R.id.iv_password_hide)
-        hidePasswordConfirmButton = view.findViewById(R.id.iv_passwordc_hide)
+        changeInputTypePasswordButton = view.findViewById(R.id.iv_password_change_input_type)
+        changeInputTypePasswordConfirmButton = view.findViewById(R.id.iv_password_confirm_change_input_type)
+
 
 
         val textWatcher = object : TextWatcher {
@@ -57,8 +59,14 @@ class RegistrationFragment : Fragment() {
         usernameEditText.addTextChangedListener(textWatcher)
         emailEditText.addTextChangedListener(textWatcher)
         passwordEditText.addTextChangedListener(textWatcher)
-        confirmEditText.addTextChangedListener(textWatcher)
+        passwordConfirmEditText.addTextChangedListener(textWatcher)
         loginButton.setOnClickListener { replaceFragment(LoginFragment()) }
+        changeInputTypePasswordButton.setOnClickListener {
+            togglePasswordVisibility(passwordEditText, changeInputTypePasswordButton)
+        }
+        changeInputTypePasswordConfirmButton.setOnClickListener {
+            togglePasswordVisibility( passwordConfirmEditText, changeInputTypePasswordConfirmButton)
+        }
         return view
     }
 
@@ -67,7 +75,7 @@ class RegistrationFragment : Fragment() {
         val username = usernameEditText.string()
         val email = emailEditText.string()
         val password = passwordEditText.string()
-        val confirmPassword = confirmEditText.string()
+        val confirmPassword =  passwordConfirmEditText.string()
         signUpButton.isEnabled = username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()
     }
 }
--- presentation/src/main/java/com/emil/presentation/utils/Extensions.kt
+++ presentation/src/main/java/com/emil/presentation/utils/Extensions.kt
@@ -1,5 +1,8 @@
 package com.emil.presentation.utils
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.widget.EditText
+import android.widget.ImageView
 import com.emil.presentation.R
 import androidx.fragment.app.Fragment
 
@@ -9,3 +12,15 @@ fun Fragment.replaceFragment(fragment: Fragment) {
     transaction.commit()
 }
 fun EditText.string():String = this.text.toString().trim()
+fun togglePasswordVisibility(passwordEditText: EditText, toggleButton: ImageView) {
+
+    if (passwordEditText.transformationMethod is PasswordTransformationMethod) {
+        passwordEditText.transformationMethod = HideReturnsTransformationMethod.getInstance()
+        toggleButton.setImageResource(R.drawable.hide_password)
+    } else {
+        passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+        toggleButton.setImageResource(R.drawable.show_password)
+    }
+    passwordEditText.setSelection(passwordEditText.text.length)
+
+}
