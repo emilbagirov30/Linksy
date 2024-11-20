f70740a390ae84dd0e64ce8789c67247316cb958
emilbagirov30 <emil_bgr@mail.ru>
2024-11-21 01:11:14 +0300

    added a dialog for creating a post

---
+++ app/src/main/java/com/emil/linksy/presentation/ui/AddPostDialogFragment.kt
@@ -0,0 +1,42 @@
+package com.emil.linksy.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.emil.presentation.R
+import com.google.android.material.appbar.MaterialToolbar
+
+class AddPostDialogFragment: DialogFragment() {
+
+    private lateinit var toolBar: MaterialToolbar
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.add_post_dialog, container, false)
+        toolBar = view.findViewById(R.id.tb_edit_data)
+        toolBar.setNavigationOnClickListener { dialog?.dismiss() }
+        return  view
+    }
+
+
+    override fun getTheme() = R.style.FullScreenDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setCancelable(false)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.apply {
+            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            setWindowAnimations(android.R.style.Animation_Dialog)
+        }
+    }
+
+}
--- app/src/main/java/com/emil/linksy/presentation/ui/PostFragment.kt
+++ app/src/main/java/com/emil/linksy/presentation/ui/PostFragment.kt
@@ -1,27 +1,43 @@
 package com.emil.linksy.presentation.ui
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.EditText
 import com.emil.presentation.R
 
 
 class PostFragment : Fragment() {
 
-
+private lateinit var newPostEditText:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+       val view = inflater.inflate(R.layout.fragment_post, container, false)
+        newPostEditText = view.findViewById(R.id.et_new_post)
+        newPostEditText.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                AddPostDialogFragment().show(parentFragmentManager, "AddPostDialogFragment")
+                true
+            } else {
+                false
+            }
+        }
+
+        return view
     }
 
 
