9270ea93cd73c04d655450a0e9fa8805b466942e
emil <milbgr@gmail.com>
2025-02-26 04:23:52 +0300

    changed versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.emil.domain"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
